d5c9908b2ea6088ec63e62d9ff3360232f084d37
Justin <justin.koningsberger@gmail.com>
2026-07-23 13:36:22 +0200

    fix(rename genarator to generator in stressTest.js)

--- stressTest.js
+++ stressTest.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const forward = require("./transforms/forward");
-const { applyWallpaperSymmetry } = require("./spiralSVGGenarator");
+const { applyWallpaperSymmetry } = require("./spiralSVGGenerator");
 
 // -------------------------------------------------------------------------
 // MASTER CONFIGURATION & GLOBAL CONTROLS
